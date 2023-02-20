4983306bf0d1a93b3861da0844f92d172b092c29
Alexbarchavero <125913095+Alexbarchavero@users.noreply.github.com>
2023-02-20 15:44:53 -0600

    Se crea clase objetos, se importa todo lo de personaje
    Se instacia Heroe
    Se imprimen los datos

--- OBJETOS.py
+++ OBJETOS.py
@@ -1,1 +1,7 @@
 from PERSONAJE import *
+#creacion de instancias de la clase personaje
+Heroe = Personaje()
+#usamos los atributos
+print("El personaje se llama: "+Heroe.nombre)
+print("Pertenece a la especie: "+Heroe.especie)
+print("Tiene una altura de: "+str(Heroe.altura))

d84bbfaed9de56af8931576b0722c32b3b109456
Alexbarchavero <125913095+Alexbarchavero@users.noreply.github.com>
2023-02-20 15:38:51 -0600

    Se agregaron los metodos faltantes
    lanzarGranadas
    recargarArma

--- PERSONAJE.py
+++ PERSONAJE.py
@@ -11,3 +11,11 @@ class Personaje:
             print("El personaje "+self.nombre+" esta corriendo")
         else:
             print("El personaje "+self.nombre+" se detuvo")
+    
+    def lanzarGranadas(self):
+        print("El personaje "+self.nombre+"lanzo la granada")
+    
+    def recargarArma(self,municiones):
+        cargador = 10
+        cargador += municiones
+        print("El arma recargada tiene "+cargador+" balas")

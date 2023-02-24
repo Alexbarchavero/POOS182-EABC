cdc227840e6ccfbc3eb3a785c80bfbab65036789
Alexbarchavero <125913095+Alexbarchavero@users.noreply.github.com>
2023-02-24 14:52:36 -0600

    Encapsulamiento

--- OBJETOS.py
+++ OBJETOS.py
@@ -5,7 +5,6 @@ especieH = input("Escribe la especie del Heroe: ")
 nombreH = input("Escribe el nombre del Heroe: ")
 altH = float(input("Escribe la altura del Heroe: "))
 recargarH = int(input("Ingresa las balas del Heroe: "))
-
 print("\n----------SOLICITUD DE DATOS DEL VILLANO----------")
 especieV = input("Escribe la especie del Villano: ")
 nombreV = input("Escribe el nombre del Villano: ")
@@ -15,21 +14,21 @@ recargarV = int(input("Ingresa las balas del Villano: "))
 #2. CREAR OBJETOS
 Heroe = Personaje(especieH, nombreH, altH)
 Villano = Personaje(especieV, nombreV, altV)
+Heroe.setNombre("Pepe pepe")
 
-#3. USAR ATRIBUTOS
-#4. USAR LOS METODOS
+#3 y 4. USAR ATRIBUTOS Y USAR LOS METODOS
 print("\n----------DATOS DEL HEROE----------")
-print("El Heroe se llama: "+Heroe.nombre)
-print("Pertenece a la especie: "+Heroe.especie)
-print("Tiene una altura de: "+str(Heroe.altura))
+print("El Heroe se llama: "+Heroe.getNombre())
+print("Pertenece a la especie: "+Heroe.getEspecie())
+print("Tiene una altura de: "+str(Heroe.getAltura()))
 Heroe.correr(True)
 Heroe.lanzarGranadas()
 Heroe.recargarArma(recargarH)
-
 print("\n----------DATOS DEL VILLANO----------")
-print("El Villano se llama: "+Villano.nombre)
-print("Pertenece a la especie: "+Villano.especie)
-print("Tiene una altura de: "+str(Villano.altura))
+print("El Villano se llama: "+Villano.getNombre())
+print("Pertenece a la especie: "+Villano.getEspecie())
+print("Tiene una altura de: "+str(Villano.getAltura()))
 Villano.correr(True)
 Villano.lanzarGranadas()
 Villano.recargarArma(recargarV)
+#Villano.__pensar()
--- PERSONAJE.py
+++ PERSONAJE.py
@@ -1,22 +1,36 @@
 class Personaje:
-    
     # Constructor
     def __init__(self,esp,name,alt):
-        self.especie = esp
-        self.nombre = name
-        self.altura = alt
-    
+        self.__especie = esp
+        self.__nombre = name
+        self.__altura = alt
     #definir metodos
     def correr(self,status):
         if(status):
-            print("El personaje "+self.nombre+" esta corriendo")
+            print("El personaje "+self.__nombre+" esta corriendo")
         else:
-            print("El personaje "+self.nombre+" se detuvo")
-    
+            print("El personaje "+self.__nombre+" se detuvo")
     def lanzarGranadas(self):
-        print("El personaje "+self.nombre+" lanzo la granada")
-    
+        print("El personaje "+self.__nombre+" lanzo la granada")
     def recargarArma(self,municiones):
         cargador = 10
         cargador += municiones
         print(f"El arma recargada tiene {cargador} balas")
+    def __pensar(self):
+        print("ando pensando")
+    
+    # GETTERS AND SETTERS
+    def getEspecie(self):
+        return self.__especie
+    def setEspecie(self,esp):
+        self.__especie = esp
+    
+    def getNombre(self):
+        return self.__nombre
+    def setNombre(self,name):
+        self.__nombre = name
+    
+    def getAltura(self):
+        return self.__altura
+    def setAltura(self,alt):
+        self.__altura = alt

28951a3bf3f9eeda81a7d0b22b03b2cc813d91ad
Alexbarchavero <125913095+Alexbarchavero@users.noreply.github.com>
2023-04-03 16:23:05 -0600

    Pestaña Actualizar y Eliminar Usuarios
    Adicion de widgets y funcionalidades de ambas pestañas

--- TKINTER Y SQL LITE/PRACTICA 15,16 y17/ControladorBD.py
+++ TKINTER Y SQL LITE/PRACTICA 15,16 y17/ControladorBD.py
@@ -20,16 +20,13 @@ class ControladorBD:
         conPlana = contra
         conPlana = conPlana.encode()
         sal = bcrypt.gensalt()
-        
         conHa = bcrypt.hashpw(conPlana,sal)
         print(conHa)
-        
         return conHa
     
     def saveUser(self,name,email,contra):
         # 1 llamar metodo conexion
         conx = self.conexionBD()
-        
         # 2 validar vacios
         if (name == "" or email == "" or contra == "") :
             messagebox.showwarning("Cuidado","Formulario incompleto")
@@ -41,7 +38,6 @@ class ControladorBD:
             conH = self.encripPass(contra)
             datos = (name,email,conH)
             SaveInSql = "insert into tbRegistrados(nombre,correo,contra) values(?,?,?)"
-            
             # 5 Ejecutar insert
             cursor.execute(SaveInSql,datos)
             conx.commit()
@@ -51,7 +47,6 @@ class ControladorBD:
     def consulUser(self,id):
         # Realizar la conexion a la BD
         conx = self.conexionBD()
-        
         # Verificar que el id no este vacio
         if id ==  '':
             messagebox.showwarning("Advertencia","Campo vacío")
@@ -61,27 +56,72 @@ class ControladorBD:
             try:
                 cursor = conx.cursor()
                 sqlSelect = " select * from tbRegistrados where id = "+id
-                
                 # Ejecutar
                 cursor.execute(sqlSelect)
                 RSuser = cursor.fetchall()
                 conx.close()
                 return RSuser
-                
             except sqlite3.OperationalError:
                 print("Error de consulta")
     
-    def mostrarUsuarios(self):
-        # Creamos la conexión con la base de datos, el cursor y la consulta
-        conx = self.conexionBD()
-        cursor = conx.cursor()
-        SQLSelectAll = "select * from tbRegistrados"
-        
-        # Obtenemos todos los usuarios registrados en la tabla 'Registrados'
-        cursor.execute(SQLSelectAll)
-        usuarios = cursor.fetchall()
-
-        # Cerramos la conexión con la base de datos
-        conx.close()
-        
-        return usuarios
+    def showUsers(self):
+        try:
+            # Creamos la conexión con la base de datos, el cursor y la consulta
+            conx = self.conexionBD()
+            cursor = conx.cursor()
+            SQLSelectAll = "select * from tbRegistrados"
+            # Obtenemos todos los usuarios registrados en la tabla 'Registrados'
+            cursor.execute(SQLSelectAll)
+            usuarios = cursor.fetchall()
+            # Cerramos la conexión con la base de datos
+            conx.close()
+            # Retornamos los valores
+            return usuarios
+        except sqlite3.OperationalError:
+            print("Error de consulta")
+    
+    def updateUsers(self,ide,name,email,contra):
+        if (ide == "" and (name == "" or email == "" or contra == "")):
+            messagebox.showwarning("Cuidado","Campos vacíos")
+        else:
+            try:
+                # Conexión a la base de datos
+                conx = self.conexionBD()
+                cursor1 = conx.cursor()
+                # Actualización del registro
+                conH = self.encripPass(contra)
+                sqlUpdates = "UPDATE tbRegistrados SET nombre = ?, correo = ?, contra = ? WHERE id = ?"
+                datos = (name, email, conH, ide)
+                cursor1.execute(sqlUpdates, datos)
+                # Confirmar los cambios y cerrar la conexión
+                conx.commit()
+                conx.close()
+                messagebox.showinfo("Exito","Datos correctamente actualizados")
+            except sqlite3.OperationalError:
+                print("Error de consulta")
+    
+    def deleteUsers(self,ide):
+        if ide=="":
+            messagebox.showwarning("Cuidado","Campo vacío")
+        else:
+            try:
+                # Conexion a la base de datos
+                conx = self.conexionBD()
+                cursor2 = conx.cursor()
+                # Eliminacion de registro
+                sqlDeletes = "DELETE FROM tbRegistrados WHERE id = ?"
+                # Preguntar al usuario si desea borrar el campo 
+                respuesta = messagebox.askyesno("Confirmar eliminación", "¿Estás seguro que deseas eliminar este registro?")
+                if respuesta:
+                    # Ejecutar la consulta SQL para eliminar el registro
+                    cursor2.execute(sqlDeletes,ide)
+                    # Confirmar los cambios y cerrar la conexión
+                    conx.commit()
+                    conx.close()
+                    messagebox.showinfo("Exito","Registro eliminado")
+                else:
+                    # Cerrar la conexion
+                    conx.close()
+                    messagebox.showwarning("Advertencia","Usuario no eliminado")
+            except sqlite3.OperationalError:
+                print("Error de consulta")
--- TKINTER Y SQL LITE/PRACTICA 15,16 y17/USUARIOSCRUD.py
+++ TKINTER Y SQL LITE/PRACTICA 15,16 y17/USUARIOSCRUD.py
@@ -3,17 +3,14 @@ from tkinter import ttk
 from tkinter import messagebox, Text
 import tkinter as tk
 from ControladorBD import *
-
 # Instancia
 c = ControladorBD()
-
 # Funciones
 def exeInsert():
     c.saveUser(Name.get(),Correo.get(),Contra.get())
 
 def exeSelectUser():
     textBus.delete("0.0", "end")
-    
     rsUser = c.consulUser(Busqueda.get())
     for user in rsUser:
         cadena = str(user[0])+" "+user[1]+" "+user[2]+" "+str(user[3])
@@ -22,71 +19,96 @@ def exeSelectUser():
     else:
         messagebox.showinfo("No encontrado","Usuario no existe en BD")
 
-
 def exeSelectAll():
-    users = c.mostrarUsuarios()
+    treeviewConsulta.delete(*treeviewConsulta.get_children())
+    users = c.showUsers()
     if users:
         for user in users:
-            cadena = f'{user[0]} {user[1]} {user[2]} {user[3]}\n'
-            textConsulta.insert(tk.END, cadena)
+            cadena = (user[0], user[1], user[2], user[3])
+            treeviewConsulta.insert("", tk.END, values=cadena)
     else:
         messagebox.showinfo("Sin datos por mostrar","No se encontraron usuarios en la BD")
 
+def exeUpdates():
+    c.updateUsers(Actualizacion.get(),Name1.get(),Correo1.get(),Contra1.get())
+
+def exeDeletes():
+    c.deleteUsers(Eliminacion.get())
+
 w1 = Tk()
 w1.title("CRUD de usuarios")
-w1.geometry("640x480")
-
+w1.geometry("960x640")
 panel = ttk.Notebook(w1)
 panel.pack(fill = 'both', expand = 'yes')
-
 pestana1 = ttk.Frame(panel)
 pestana2 = ttk.Frame(panel)
 pestana3 = ttk.Frame(panel)
 pestana4 = ttk.Frame(panel)
+pestana5 = ttk.Frame(panel)
 
 # Pestaña 1
-titu = Label(pestana1,text="Registro Usuarios",fg="blue",font=("Modern",18)).pack()
-
+titu = Label(pestana1,text="Registrar Usuarios",fg="blue",font=("Century Gothic",18)).pack()
 Name = tk.StringVar()
 LBName = Label(pestana1,text="Nombre: ").pack()
 ENName = Entry(pestana1,textvariable=Name).pack()
-
 Correo = tk.StringVar()
 LBCorreo = Label(pestana1,text="Correo: ").pack()
 ENCorreo = Entry(pestana1,textvariable=Correo).pack()
-
 Contra = tk.StringVar()
 LBContra = Label(pestana1,text="Contraseña: ").pack()
 ENContra = Entry(pestana1,textvariable=Contra).pack()
-
 btnGuardar = Button(pestana1,text="Guardar Usuario",command=exeInsert).pack()
 
 # Pestaña 2
-titu2 = Label(pestana2,text="Buscar usuario",fg="green",font=("Modern",18)).pack()
-
+titu2 = Label(pestana2,text="Buscar usuario",fg="green",font=("Century Gothic",18)).pack()
 Busqueda = tk.StringVar()
 LBid = Label(pestana2,text="Identificador de usuario").pack()
 ENid = Entry(pestana2,textvariable=Busqueda).pack()
-
 btnBusqueda = Button(pestana2,text="Buscar usuario",command=exeSelectUser).pack()
-
-subBus = Label(pestana2,text="Registro",fg="blue",font=("Modern",18)).pack()
+subBus = Label(pestana2,text="Registro",fg="blue",font=("Century Gothic",18)).pack()
 textBus = Text(pestana2, height=5, width=75)
 textBus.pack()
 
 # Pestaña 3
-titu3 = Label(pestana3,text="Consultar Usuarios",fg="#8B0000",font=("Modern",18)).pack()
-
+titu3 = Label(pestana3,text="Consultar Usuarios",fg="#8B0000",font=("Century Gothic",18)).pack()
 btnConsultar = Button(pestana3,text="Consultar",command=exeSelectAll).pack()
+subtConsulta = Label(pestana3,text="Lista de usuarios",fg="black",font=("Century Gothic",18)).pack()
+treeviewConsulta = ttk.Treeview(pestana3, columns=('Id', 'Nombre', 'Correo', 'Contraseña'),show="headings")
+treeviewConsulta.heading('#0', text='Index')
+treeviewConsulta.heading('Id', text='ID')
+treeviewConsulta.heading('Nombre', text='Nombre')
+treeviewConsulta.heading('Correo', text='Correo')
+treeviewConsulta.heading('Contraseña', text='Contraseña')
+treeviewConsulta.pack()
 
-subtConsulta = Label(pestana3,text="Lista de usuarios",fg="dark gray",font=("Modern",18)).pack()
-textConsulta = Text(pestana3, height=15, width=75)
-textConsulta.pack()
+# Pestaña 4
+titu4 = Label(pestana4,text="Actualizar",fg="#EC5900",font=("Century Gothic",18)).pack()
+Actualizacion = tk.StringVar()
+LBide = Label(pestana4,text="Identificador de usuario").pack()
+ENide = Entry(pestana4,textvariable=Actualizacion).pack()
+Name1 = tk.StringVar()
+LBName1 = Label(pestana4,text="Nombre: ").pack()
+ENName1 = Entry(pestana4,textvariable=Name1).pack()
+Correo1 = tk.StringVar()
+LBCorreo1 = Label(pestana4,text="Correo: ").pack()
+ENCorreo1 = Entry(pestana4,textvariable=Correo1).pack()
+Contra1 = tk.StringVar()
+LBContra1 = Label(pestana4,text="Contraseña: ").pack()
+ENContra1 = Entry(pestana4,textvariable=Contra1).pack()
+btnActualizar = Button(pestana4,text="Actualizar",command=exeUpdates).pack()
 
+# Pestaña 5
+titu5 = Label(pestana5,text="Eliminar",fg="#E69D00",font=("Century Gothic",18)).pack()
+Eliminacion = tk.StringVar()
+LBide = Label(pestana5,text="Identificador de usuario").pack()
+ENide = Entry(pestana5,textvariable=Eliminacion).pack()
+btnEliminar = Button(pestana5,text="Eliminar",command=exeDeletes).pack()
 
-panel.add(pestana1, text = "Agregar Usuarios")
+# Nombre de las pestañas en el panel
+panel.add(pestana1, text = "Añadir Usuarios")
 panel.add(pestana2, text = "Buscar Usuario")
 panel.add(pestana3, text = "Consultar Usuarios")
 panel.add(pestana4, text = "Actualizar Usuario")
+panel.add(pestana5, text = "Eliminar Usuario")
 
 w1.mainloop()
